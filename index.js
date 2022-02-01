aebfc7fa1e2909b1a6d71abb634c3f7411b51cbf
JustinPooters <justin@justinpooters.nl>
2022-02-01 21:14:20 +0100

    Added getServerDetails API call, i need to find a way to define global variable (API-Key and API-URL).... 😥

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ async function adminGetServers(url, apikey) {
   return response.data.data;
 };
 
+async function getServerDetails(url, apikey, id) {
+  const axios = require("axios");
+  const response = await axios.get(`https://${url}/api/application/servers/${id}`, {
+    headers: {
+      'Authorization': `Bearer ${apikey}`,
+      'Accept': 'application/json',
+      'Content-Type': "application/json"
+    }
+  }).catch(error => { return error; })
+  return response.data;
+};
+
 module.exports = {
-  adminGetServers
+  adminGetServers,
+  getServerDetails
 }
